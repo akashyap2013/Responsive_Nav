bdde4df16590ffea035f3fcd9a80930ee2345f2c
akashyap2013 <akshaykashyap.2013@gmail.com>
2019-09-07 20:59:11 +0530

    commit

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggleCollapse = document.querySelector('.toggle-icons span');
-const nav = document.querySelector('.nav');
-
-toggleCollapse.onclick = (e) => {
-    // nav.classList.add('collapse');
-    nav.classList.toggle("collapse");
-    e.target.classList.toggle("toggle-click");
-}
